13f273e33a10474a875efeb6e11539f438b515f6
Mahmudul-Amin-Minar <Mahmudul.minar@gmail.com>
2017-03-24 06:17:29 +0600

    URI

---
+++ 1050-DDD.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==61){
+        printf("Brasilia");
+    }
+    else if(a==71){
+        printf("Salvador");
+    }
+    else if(a==11){
+        printf("Sao Paulo");
+    }
+    else if(a==21){
+        printf("Rio de Janeiro");
+    }
+    else if(a==32){
+        printf("Juiz de Fora");
+    }
+    else if(a==19){
+        printf("Campinas");
+    }
+    else if(a==27){
+        printf("Vitoria");
+    }
+    else if(a==31){
+        printf("Belo Horizonte");
+    }
+    else{
+        printf("DDD nao cadastrado");
+    }
+    printf("\n");
+}
